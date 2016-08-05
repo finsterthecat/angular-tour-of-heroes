df54fd2fadfa26914303957d6bc64bb3c311c7fb
Tony Brouwer <tonybrouwer@gmail.com>
2016-08-05 16:27:16 -0400

    Just before add/modify/delete

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,8 +10,10 @@ import { DashboardComponent } from './dashboard.component';
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
+    <nav>
     <a [routerLink]="['/dashboard']" routerLinkActive="active">Dashboard</a>
     <a [routerLink]="['/heroes']" routerLinkActive="active">Heroes</a>
+    </nav>
     <router-outlet></router-outlet>
   `,
   styleUrls: ['app/app.component.css'],
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,19 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http'
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+import 'rxjs/add/operator/toPromise';
+
 @Injectable()
 export class HeroService {
+  private heroesUrl = 'app/heroes';  // URL to web api
+
+  constructor(private http: Http) { }
+
   getHeroes() {
-    return Promise.resolve(HEROES);
-  };
+    return this.http.get(this.heroesUrl)
+               .toPromise()
+               .then(response => response.json().data as Hero[])
+               .catch(this.handleError);
+  }
   getHero(id: number) {
     return this.getHeroes()
                .then(heroes => heroes.find(hero => hero.id === id));
   }
-  getHeroesSlowly() {
-    return new Promise<Hero[]>(resolve =>
-      setTimeout(() => resolve(HEROES), 2000) // 2 seconds
-    );
+  private handleError(error: any) {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
   }
+
 }
 
