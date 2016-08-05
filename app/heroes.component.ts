daf2fc7e1aadd08418029675d3eb9a2e7cea8e9b
Tony Brouwer <tonybrouwer@gmail.com>
2016-08-05 17:17:36 -0400

    Mostly working save for when adding Heros where add form doesn't disappear

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -8,22 +8,49 @@ import { Router } from '@angular/router';
   selector: 'my-heroes',
   templateUrl: 'app/heroes.component.html',
   styleUrls: ['app/heroes.component.css'],
-  providers: []
+  directives: [HeroDetailComponent]
 })
 
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
+  error: any;
+
   constructor(
     private router: Router,
     private heroService: HeroService) { }
+
   getHeroes() {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
+
+  addHero() {
+    this.addingHero = true;
+    this.selectedHero = null;
+  }
+
+  deleteHero(hero: Hero, event: any) {
+    event.stopPropagation();
+    this.heroService
+        .delete(hero)
+        .then(res => {
+          this.heroes = this.heroes.filter(h => h !== hero);
+          if (this.selectedHero === hero) { this.selectedHero = null; }
+        })
+        .catch(error => this.error = error);
+  }
+
+  close(savedHero: Hero) {
+    this.addingHero = false;
+    if (savedHero) { this.getHeroes(); }
+  }
+
   ngOnInit() {
     this.getHeroes();
   }
+
   onSelect(hero: Hero) { this.selectedHero = hero; }
+
   gotoDetail() {
     this.router.navigate(['/detail', this.selectedHero.id]);
   }
