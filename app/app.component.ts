bdc4b07809d7c5b21b114711136c819663615d65
Tony Brouwer <tonybrouwer@gmail.com>
2016-08-05 15:24:10 -0400

    Finished routes section

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,20 +3,25 @@ import { ROUTER_DIRECTIVES } from '@angular/router'
 
 import { HeroService }     from './hero.service';
 import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
 
 @Component({
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <a [routerLink]="['/heroes']">Heroes</a>
+    <a [routerLink]="['/dashboard']" routerLinkActive="active">Dashboard</a>
+    <a [routerLink]="['/heroes']" routerLinkActive="active">Heroes</a>
     <router-outlet></router-outlet>
   `,
+  styleUrls: ['app/app.component.css'],
   directives: [ROUTER_DIRECTIVES],
   providers: [
     HeroService
   ],
-  precompile: [HeroesComponent]
+  precompile: [DashboardComponent, HeroesComponent, HeroDetailComponent]
 })
+
 export class AppComponent {
   title = 'Tour of Heroes';
 }
--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,7 +1,22 @@
 import { provideRouter, RouterConfig }  from '@angular/router';
 import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 const routes: RouterConfig = [
+  {
+    path: 'detail/:id',
+    component: HeroDetailComponent
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
   {
     path: 'heroes',
     component: HeroesComponent
